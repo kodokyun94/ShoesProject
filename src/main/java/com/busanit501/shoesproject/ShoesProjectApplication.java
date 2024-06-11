c1c45fb426cba39f77f58ad720ac5562dbc34ab7
moonjungseob <answjdtjq11@gmail.com>
2024-06-12 01:42:27 +0900

    first commit

---
+++ src/main/java/com/busanit501/shoesproject/ShoesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.busanit501.shoesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoesProjectApplication.class, args);
+    }
+
+}
