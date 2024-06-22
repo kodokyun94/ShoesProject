2e22c93a19470124eaf0edeaaec97cdde9a582d4
ghwls3995 <ghwls3995@gmail.com>
2024-06-22 20:55:05 +0900

    6.22 pr 현재 상품평관련하여 productpage 에 제품상세페이지 하단에 상품평이 있는데 관련하여 우선 html 작업좀 하였음. 하면서 footer 작업이랑 반응형 웹작업도 같이 추가할려는 상황 footer 는 base에는 잘작동하는데 다른 page에서 위치가 겹치는 이슈로 css좀 만져야 하는상황이며 , 반응형웹작업은 이 문제 해결하고 할려고 합니다. shoescolor같은경우는 어떻게 해야할지 애매해서 table만 만들어놓은 상황이고 shoessize는 나중에 제품을 선택하여 장바구니로 넘겨야하는상황에 있어야할 거 같아서 우선 table에 size_id 와 item_size 더미값을 미리 넣어놓은 상황 nhj브랜치에 txt파일 참고 웹에서 상품평 넘겨받아 출력하는 과정 할 예정 + 제품list 보여주는 페이지 작업해야함

--- src/main/java/com/busanit501/shoesproject/dto/nhjdto/ReviewDTO.java
+++ src/main/java/com/busanit501/shoesproject/dto/nhjdto/ReviewDTO.java
@@ -13,8 +13,12 @@ public class ReviewDTO {
 
     private long item_id;
     private long member_id;
+    // member id를 정수형으로 받을건지 string으로 받을지도 얘기해봐야할거같아요.
+
     private String member_name;
     private String item_rev;
-    private String item_rank;
+    // 리뷰를 여기 선언하게 된다면 범위(글 제한 수) 생각해봐야할 듯.
 
+    private String item_rank;
+    // 평점 추가할것인지 생각해봐야함
 }
