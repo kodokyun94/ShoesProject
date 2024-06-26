87a50d19efe1a72ff2914e7c307b7032ffa87fc4
LeeSooJin <gowl0918@gmail.com>
2024-06-26 10:36:18 +0900

    이수진 서비스 테스트1 생성

---
+++ src/test/java/com/busanit501/shoesproject/ServiceTests/LSJServiceTests/ShoesServiceTest1.java
@@ -0,0 +1,12 @@
+package com.busanit501.shoesproject.ServiceTests.LSJServiceTests;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class ShoesServiceTest1 {
+
+
+}
