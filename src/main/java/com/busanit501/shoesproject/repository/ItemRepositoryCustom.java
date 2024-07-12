c7149a5e13647fb58ed0b15451c7d73c369654ed
KODOKYUN <hwasungnj3@gmail.com>
2024-07-12 18:31:22 +0900

    0710 쥐에에엔장 실행을 위한 여정 3 domain(entity) 수정 완료 repository 수정 완료(dto 수정 후 관련 된거 확인 해야 함 (완료)) security 수정 완료 dto 수정 완료 config 수정 중

--- src/main/java/com/busanit501/shoesproject/repository/ItemRepositoryCustom.java
+++ src/main/java/com/busanit501/shoesproject/repository/ItemRepositoryCustom.java
@@ -2,8 +2,8 @@ package com.busanit501.shoesproject.repository;
 
 
 import com.busanit501.shoesproject.domain.Item;
-import com.busanit501.shoesproject.dto.kdkdto.ItemSearchDto;
-import com.busanit501.shoesproject.dto.kdkdto.MainItemDto;
+import com.busanit501.shoesproject.dto.ItemSearchDto;
+import com.busanit501.shoesproject.dto.MainItemDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
