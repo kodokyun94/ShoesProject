b57367f7c8adc1a35ab5108e374c6ff924623d22
KODOKYUN <hwasungnj3@gmail.com>
2024-07-23 19:47:28 +0900

    0723 쥐에에엔장 실행을 위한 여정 7 엔티티랑 레파지토리는 이해했음 서비스랑 컨트롤러, 화면 공부 필요할 듯
    오류는 수정 중

--- src/main/java/com/busanit501/shoesproject/repository/shop/CartItemRepository.java
+++ src/main/java/com/busanit501/shoesproject/repository/shop/CartItemRepository.java
@@ -12,7 +12,7 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
 
-    @Query("select new com.busanit501.shoesproject.dto.CartDetailDTO(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " +
+    @Query("select new com.busanit501.shoesproject.dto.shop.CartDetailDTO(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " +
             "from CartItem ci, ItemImg im " +
             "join ci.item i " +
             "where ci.cart.id = :cartId " +
